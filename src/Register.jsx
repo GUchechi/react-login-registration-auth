0a642c7dca4d999a127f54e4107adc7e912bb486
GUchechi <godswilluchendu7@gmail.com>
2022-10-14 13:03:11 +0100

    first commit

---
+++ src/Register.jsx
@@ -0,0 +1,12 @@
+import {React, useRef, useState, useEffect} from 'react'
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
