80f65834029d3924867e89605e3ecb1636141f6f
GUchechi <godswilluchendu7@gmail.com>
2022-10-18 00:46:09 +0100

    added Login file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Register from './Register';
+import Login from './Login';
+// import Register from './Register';
 
 function App() {
   return (
     <main className="App">
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </main>
   );
 }
